ff2128826d978bf9ef3a819ff49403bca6bdb84e
mammarraza <ammarqadri280@gmail.com>
2025-03-23 06:46:23 +0500

    test

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.use("/", frontRouter);
 
 // Start server
 const PORT = process.env.PORT || 5001;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-module.exports.handler = serverless(app);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+// module.exports.handler = serverless(app);
